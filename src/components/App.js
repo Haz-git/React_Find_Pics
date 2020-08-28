7bc2bf35e1c0a82f91e92201deaa7b2349ca8433
Haz-git <zhouharry1@gmail.com>
2020-08-28 09:56:01 -0700

    Updated Feature To Access Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const userResponse = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID paonAu4H4KeeY_qKsyTn4Js_M9C8w-kqBZXfyraLrdk' 
+            }
+        });
+
+        console.log(userResponse.data.results);
     }
 
     render() {
